f1ebff25bab0a044c5ff31a23e55b937240e654f
Breno <professionalbreno@gmail.com>
2026-09-07 11:31:46 -0300

    add `database.js` with the use of environment variables

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,8 @@
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("select 1+ 1;");
+  response.status(200).json({ chave: "são acima da média" });
+}
+
+export default status;
