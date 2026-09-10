886437e51a100ca0fed45171be48c1900f0394c6
Breno <professionalbreno@gmail.com>
2026-09-10 11:33:57 -0300

    `database.js` made more robust to error handling.
    Finished
    `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,33 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("select 1+ 1;");
-  response.status(200).json({ chave: "são acima da média" });
+  const updatedAt = new Date().toISOString();
+  const versionRequest = await database.query("SHOW server_version;");
+
+  const version = versionRequest.rows[0].server_version;
+
+  const maxConnectionsRequest = await database.query("SHOW max_connections;");
+
+  const maxConnections = parseInt(
+    maxConnectionsRequest.rows[0].max_connections,
+  );
+
+  const databaseName = process.env.POSTGRES_DB;
+  const result = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const openedConnections = result.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      version: version,
+      maxConnections: maxConnections,
+      openedConnections: openedConnections,
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,16 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.dependencies.version).toBeDefined();
+  expect(responseBody.dependencies.maxConnections).toBeDefined();
+  expect(responseBody.dependencies.openedConnections).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(responseBody.dependencies.version).toBe("16.0");
+  expect(responseBody.dependencies.maxConnections).toBe(100);
+  expect(responseBody.dependencies.openedConnections).toBe(1);
 });
